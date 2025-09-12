c5280ae8a4d0e47dd0dc5d7e27d8849583f30c83
duckii <duckiiduckie@gmail.com>
2025-09-12 21:47:14 +0700

    remove unused file

--- src/main/java/com/example/backend/dto/request/progress/LessonProgressRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.dto.request.progress;
-
-import lombok.Data;
-
-@Data
-public class LessonProgressRequest {
-    // No additional fields needed - lesson ID comes from path parameter
-    // Status will be automatically set to COMPLETE
-}
